6b345fdba17a039532aeb72578f1f5b33147ffa7
FellipeMed <=>
2024-08-20 20:06:44 -0400

    nada de mais

---
+++ desafio1.js
@@ -0,0 +1,15 @@
+//funcão que calcula a media de uma quantidade indefina de argumentos
+function calcMedia(){
+    let arg = arguments
+    var soma = 0
+
+    for(var i = 0; i < arg.length; i++){
+        soma += arg[i]
+    }
+
+    var media = soma/arg.length
+    return media
+}
+console.log("-----------------------------------")
+console.log("RESPOSTA: " + calcMedia(2, 5, 7, 1, -2))
+console.log("-----------------------------------")
