a6497da53f7d9467a7a53d045a3d984d07b85a60
FellipeMed <=>
2024-08-21 21:38:17 -0400

    desafio 2

---
+++ desafio2.js
@@ -0,0 +1,17 @@
+// Esse desafio consiste em usar recursão,
+// o desafio proposto é imprimir "chunk" usando recursão sem usar algum laço de repetição
+// Por exemplo:
+// se a entrada num(4) então o resultado deve ser "chunk-chunk-chunk-chunk"
+var name = "Chunk" 
+function chunks(n){
+    let number = n
+    
+
+    if(number>1){
+        name += "-chunk"
+        chunks(number-1)
+    }
+    return name
+}
+
+console.log(chunks(4))
